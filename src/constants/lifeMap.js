ba9adace66b74b358483d68e414752544a2c1682
slusarevzahar-collab <slusarevzahar@gmail.com>
2026-06-22 15:43:35 +0300

    Add LifeMap constants

---
+++ src/constants/lifeMap.js
@@ -0,0 +1,19 @@
+export const FOCUS_STORAGE_KEY = 'lifeMapFocusQueueV3';
+export const TITLE_ALIASES_KEY = 'lifeMapTitleAliasesV2';
+export const DRAG_THRESHOLD = 8;
+
+export const RENAMABLE_KINDS = new Set([
+  'task',
+  'goal',
+  'project',
+  'lifeArea',
+  'signal',
+  'dream',
+  'sphere',
+]);
+
+export const mapVariants = {
+  initial: { opacity: 0, scale: 0.9 },
+  animate: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 1.06 },
+};
