c60db75f613f3e5cb728617e78ea773e5518bab5
slusarevzahar-collab <slusarevzahar@gmail.com>
2026-07-06 12:42:30 +0300

    Define LifeMap AI policy constants

--- server/lifemapAiPolicy.js
+++ server/lifemapAiPolicy.js
@@ -1,1 +1,15 @@
-placeholder
+export const AI_POLICY_VERSION = '2026-07-06.1';
+
+export const EXECUTABLE_ACTIONS = [
+  'update_task',
+  'rename_item',
+  'create_session',
+  'create_signal',
+  'dedupe_signals',
+];
+
+export const PLANNING_ACTIONS = [
+  'frontend_change_request',
+  'backend_change_request',
+  'research_request',
+];
